65331270b60318d6d0d0408318f988618dd58c75
gelnapet <gelnapet@WW011173.eu.tieto.com>
2012-01-27 13:03:37 +0100

    initial

---
+++ TestGit/src/cz/tor/testgit/Main.java
@@ -0,0 +1,13 @@
+package cz.tor.testgit;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println();
+	}
+
+}
